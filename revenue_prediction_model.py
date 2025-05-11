2a370d738e477e8d6ad17a46477dd92d6abec9b0
Sunny <sanidhyakhanna123@gmail.com>
2025-05-12 03:41:13 +0400

    commit final changs

--- revenue_prediction_model.py
+++ revenue_prediction_model.py
@@ -9,9 +9,9 @@ import warnings
 warnings.filterwarnings('ignore')
 
 def prepare_data():
-    df = pd.read_csv(r"7edge-revenue-predictions/revenue_predictions.csv")
+    df = pd.read_csv(r"C:\Users\sanid\OneDrive\Desktop\7EDGE PREEDICTION MODEL\7edge-revenue-predictions\synthetic_revenue_data.csv")
     df.columns = df.columns.str.strip()
-    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=False, errors='coerce')
     df.set_index('Date', inplace=True)
     df = df.asfreq('D')
     df['MA7'] = df['Revenue'].rolling(window=7).mean()
